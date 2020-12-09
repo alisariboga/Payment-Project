4ac5dc660b390c707491955b7335d77c3bd009b8
alsrbga <alican.sariboga.acs@gmail.com>
2020-12-09 21:18:42 +0000

    Firs API

---
+++ src/main/java/com/payroll/LittleProject/LittleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.payroll.LittleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleProjectApplication.class, args);
+	}
+
+}
