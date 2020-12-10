96668de487b9b07a75783aeddc9d0eb6528d3402
alsrbga <alican.sariboga.acs@gmail.com>
2020-12-10 17:06:05 +0000

    Some Fixed

--- src/main/java/com/payroll/PaymentProject/PaymentProjectApplication.java
+++ src/main/java/com/payroll/PaymentProject/PaymentProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LittleProjectApplication {
+public class PaymentProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LittleProjectApplication.class, args);
+		SpringApplication.run(PaymentProjectApplication.class, args);
 	}
 
 }
