84116fca889612cbff4ae1bc1a1bf16c0ad3b07b
alsrbga <alican.sariboga.acs@gmail.com>
2020-12-10 11:44:53 +0000

    Some Fixed

---
+++ src/main/java/com/payroll/LittleProject/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.payroll.LittleProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+class EmployeeController {
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee){
+        return repository.save(newEmployee);
+    }
+
+
+}
